4c2f105bf4ada2e38fad1be8e5dc65717301560b
Chris Bartek, Jr <cgbartek@gmail.com>
2020-04-12 10:44:03 -0400

    Basic array separation

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $dbname = "sweetwater_test";
 $db = new mysqli($server, $username, $password, $dbname);
 
 if ($db->connect_error) {
-    die("DB connect failed: " . $db->connect_error);
+  die("DB connect failed: " . $db->connect_error);
 }
 
 ?>
--- task1.php
+++ task1.php
@@ -6,13 +6,30 @@ require "config.php";
 $sql = "SELECT orderid, comments FROM sweetwater_test";
 $result = $db->query($sql);
 
+$candy = [];
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row['orderid']. " - comments: " . $row['comments'] . "<br>";
+  while($row = $result->fetch_assoc()) {
+    if(stripos($row['comments'], 'candy') !== false || stripos($row['comments'], 'smarties') !== false) {
+      $candy[] = $row;
+    } else if(stripos($row['comments'], 'call') !== false) {
+      $call[] = $row;
+    } else if(stripos($row['comments'], 'referred') !== false) {
+      $referred[] = $row;
+    } else if(stripos($row['comments'], 'signature') !== false) {
+      $signature[] = $row;
+    } else {
+      $misc[] = $row;
     }
+    //echo "id: " . $row['orderid']. " - comments: " . $row['comments'] . "<br>";
+  }
 } else {
-    echo "No results.";
+  echo "No results.";
 }
-$conn->close();
+
+echo '<pre>';
+print_r($candy);
+echo '</pre>';
+
+$db->close();
 
  ?>
