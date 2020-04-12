c0ebe66467ee1dc7ac29028c68f0244a335bdce1
Chris Bartek, Jr <cgbartek@gmail.com>
2020-04-12 11:25:07 -0400

    Task2 table update

--- task2.php
+++ task2.php
@@ -11,15 +11,19 @@ if ($result->num_rows > 0) {
     $datepos = strpos($row['comments'], 'Expected Ship Date: ');
     $shipdate = substr($row['comments'], $datepos + 20, 9);
     if($datepos !== false) {
-      echo "Found Ship Date $shipdate on id $row[orderid].<br>";
+      echo "Found Ship Date $shipdate on id $row[orderid]... ";
+      $sqldate = date("Y-m-d",strtotime($shipdate));
+      $sql_update = "UPDATE sweetwater_test SET shipdate_expected='$sqldate' WHERE orderid='$row[orderid]'";
+      if ($db->query($sql_update) === TRUE) {
+        echo "Record updated.<br>";
+      } else {
+        echo "ERROR: " . $db->error . "<br>";
+      }
     }
   }
 } else {
   echo "No results.";
 }
 
-
-
 $db->close();
-
- ?>
+?>
